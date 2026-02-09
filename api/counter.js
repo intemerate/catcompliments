3d052c2f52cfd45ec5af2f1a93cce78cc2281009
$torm <stormzzyyne@gmail.com>
2026-02-09 11:03:05 +0200

    hmm

--- api/counter.js
+++ api/counter.js
@@ -1,21 +1,45 @@
-var Counter = require('counterapi').Counter;
-
-var counter = new Counter(process.env.COUNTER_API_TOKEN);
+var https = require('https');
 
 module.exports = function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
   
-  if (req.method === 'GET') {
-    counter.get('compliments', 'total').then(function(result) {
-      res.json({ count: result.data });
-    }).catch(function(error) {
-      res.status(500).json({ error: error.message });
+  var token = process.env.COUNTER_API_TOKEN;
+  var options = {
+    hostname: 'api.counterapi.dev',
+    path: '/v1/compliments/total',
+    method: req.method === 'POST' ? 'POST' : 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + token,
+      'Content-Type': 'application/json'
+    }
+  };
+  
+  var apiReq = https.request(options, function(apiRes) {
+    var data = '';
+    apiRes.on('data', function(chunk) {
+      data += chunk;
     });
-  } else if (req.method === 'POST') {
-    counter.up('compliments', 'total').then(function(result) {
-      res.json({ count: result.data });
-    }).catch(function(error) {
-      res.status(500).json({ error: error.message });
+    apiRes.on('end', function() {
+      try {
+        var result = JSON.parse(data);
+        res.statusCode = 200;
+        res.end(JSON.stringify({ count: result.count || result.value || 0 }));
+      } catch (e) {
+        res.statusCode = 500;
+        res.end(JSON.stringify({ error: 'Parse error' }));
+      }
     });
+  });
+  
+  apiReq.on('error', function(e) {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ error: e.message }));
+  });
+  
+  if (req.method === 'POST') {
+    apiReq.write(JSON.stringify({ action: 'increment' }));
   }
+  
+  apiReq.end();
 };
